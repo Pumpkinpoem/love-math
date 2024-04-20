15df3d7e7366f6ed596b312b7ac083b4a7c8e5f2
Andreas Almen <andreas.almen@live.com>
2024-04-20 13:38:17 +0000

    set up structure game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incementScore() {
+
+}
+
+function incementWrongAnswer () {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
